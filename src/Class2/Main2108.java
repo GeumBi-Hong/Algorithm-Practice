847e6ca5af914ef205c85c30f565416433e9a88a
GeumBi-Hong <canbe159@gmail.com>
2022-01-17 23:54:25 +0900

    통계학

--- src/Class2/Main2108.java
+++ src/Class2/Main2108.java
@@ -1,11 +1,10 @@
 package Class2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-
-public class Main2108 {
+public class Main2018 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,64 +12,49 @@ public class Main2108 {
 
         int N = Integer.parseInt(br.readLine());
         int[] num = new int[N];
+        int max=0;
 
         int [] count =  new int[8001]; // [0] = -4000  [4000] = 0 [8000] =4000
         double sum = 0;
-        int mode_max=0;
-        int max= Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
 
         for (int i = 0; i <N ; i ++){
             int v = Integer.parseInt(br.readLine());
             num[i]=v;
             count[v+4000] ++; //수가 몇번 나왔는지 세어준다.
 
-            if( max <v){
-                max =v;
-            }
-            if (min > v){
-                min = v;
-            }
-
-            if(count[v+4000]>mode_max){
-                mode_max=count[v+4000];
+            if(count[v+4000]>max){
+                max=count[v+4000];
             }
 
             sum += num[i];
         }
 
-
+        Arrays.sort(num);
 
         int c =0;
-        int mid_index=0;
-        int mode =0;
-        int mid =0;
-        for(int i = min +4000 ;i <=max +4000;i++){
+        int index =-1;
+        for(int i = 0 ;i <count.length;i++){
+            if(count[i]==max){
+                index=i;
+                c++;
+            }
 
-           if(count[i]>0) {
+            if( c ==2){ //여러개 최빈값중 두번쨰로 작은 값을 만나면 index 값을 저장하고 빠져나간다.
+                index=i;
+                break;
+            }
 
-               if (count[i] == mode_max) { //최빈값 찾기
-                  if(c<2){
-                      c++;
-                      mode = i -4000;
-                  }
-               }
-
-               if ( mid_index == N/2){ //중앙값 찾기
-                   mid = i-4000;
-               }
-               mid_index++;
-           }
         }
 
 
         sb.append(String.format("%.0f",sum/N)).append("\n"); //산술평균 (소수 첫재짜리 반올림)
-        sb.append(mid).append("\n"); //중앙값
-        sb.append(mode).append("\n"); //최빈값
-        sb.append(max -min); // 범위
+        sb.append(num[N/2]).append("\n"); //중앙값
+        sb.append(index-4000).append("\n"); //최빈값
+        sb.append(num[num.length - 1] - num[0]); // 범위
 
         System.out.print(sb);
 
 
     }
 }
+

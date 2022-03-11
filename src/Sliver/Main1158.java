05eb71febd5547b68e1aefc8ae37f995fef3d927
GeumBi-Hong <canbe159@gmail.com>
2022-03-11 23:54:24 +0900

    요세푸스

--- src/Sliver/Main1158.java
+++ src/Sliver/Main1158.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
-
+//요세푸스
 public class Main1158 {
     public static void main(String[] args) throws IOException {
 

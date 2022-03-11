3946d394e398928c2d0241fbb412800ca122376a
GeumBi-Hong <canbe159@gmail.com>
2022-03-11 23:54:01 +0900

    요세푸스

--- src/Main2012.java
+++ src/Main2012.java
@@ -1,6 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+public class Main2012 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int rank = 1;
+        long answer= 0; // 합을 구하는 과정에서  int 범위를 넘을 수있음
+
+        int [] array = new int[500001];
+
+        while (N-->0){
+            array[Integer.parseInt(br.readLine())]++;
+        }
+
+
+        for(int i = 1 ; i<=500000;i++){
+            for(int j = 0 ;j<array[i];j++){
+                if(rank==N+1) break;
+                answer+= Math.abs(i-rank);
+
+                rank++;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 //등수 매기기 2012 (그리디).
 public class Main2012 {
@@ -27,3 +60,4 @@ public class Main2012 {
 
     }
 }
+*/
---
+++ src/Sliver/Main1158.java
@@ -0,0 +1,37 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main1158 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer> list  = new LinkedList<>();
+
+        for (int i = 1 ; i <=N; i++){
+            list.add(i);
+        }
+        int index=0;
+        sb.append('<');
+        while (N>1){
+            index = (index + (K-1)) % N;
+            sb.append(list.remove(index)).append(",");
+
+            N--;
+
+        }
+
+        sb.append(list.remove()).append('>');
+       System.out.print(sb);
+    }
+}

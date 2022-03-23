fde2cc3e8b27dda622d54450531fe26658d4eeab
GeumBi-Hong <canbe159@gmail.com>
2022-03-23 23:16:18 +0900

    날짜계산

--- src/Sliver/Main1476.java
+++ src/Sliver/Main1476.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//날짜계산
 public class Main1476 {
 
     public static void main(String[] args) throws IOException {

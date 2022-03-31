47671df056e1f33f39d5761d11b11e793950ee75
GeumBi-Hong <canbe159@gmail.com>
2022-03-31 23:44:28 +0900

    N과 M (4) - [ 백트래킹 ]

---
+++ src/Gold/Main15652.java
@@ -0,0 +1,43 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+    static  int N,M;
+    static int [] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); //출력 수
+
+        arr= new int[M];
+
+        backTracking(0,1);
+
+        System.out.print(sb);
+
+    }
+
+    public static void backTracking(int depth ,int start){
+
+        if(depth==M){
+            for(int n : arr){
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start ; i<=N;i++){
+            arr[depth]=i;
+            backTracking(depth+1,i);
+        }
+    }
+}

10c278909419c2d963a3c52061339aca73f54a5e
GeumBi-Hong <canbe159@gmail.com>
2022-06-10 22:32:32 +0900

    애드 훅 - [수학 ,정수론 ,애드 훅]

---
+++ src/Sliver/Main15736.java
@@ -0,0 +1,20 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main15736 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        for(int i =1;i*i<=N;i++){
+            answer++;
+        }
+
+
+        System.out.println(answer);
+    }
+}

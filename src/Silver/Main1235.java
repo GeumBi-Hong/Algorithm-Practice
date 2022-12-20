d182a8a4799e5a17c6f3cfa8aa1b4c6aad9e9a6d
GeumBi-Hong <canbe159@gmail.com>
2022-12-20 22:36:06 +0900

    학생번호 -[구현 ,문자열]

---
+++ src/Silver/Main1235.java
@@ -0,0 +1,70 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main1235 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+
+        String str[] = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            str[i] = br.readLine();
+        }
+
+
+        //이분탐색을 이용해서 k를 구해봅시다.
+
+        //1먼저 이분 탐색의 범위를 구해봅시다
+
+        int left = 0; //최소 1자리는 되야하고
+        int right = str[0].length()-1; //문자열의 길이 만큼이 최대
+        int length = str[0].length();
+
+        int answer = -1;
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+
+            if (canDiff(mid,length,str)) {
+                //서로 고유 번호라면
+
+                left = mid + 1;
+                answer = mid;
+            }else {
+
+                right= mid -1;
+            }
+        }
+
+        System.out.println(length-answer);
+
+    }
+
+    private static boolean canDiff (int k , int l,String []number) {
+
+        //핵심로직
+
+        //substring정리좀 하자.
+        Map<String,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < number.length;i++){
+            map.put(number[i].substring(k),0);
+        }
+
+        if(map.size()==number.length)return true;
+        else return false;
+
+
+
+    }
+    }
---
+++ src/Silver/Main12873.java
@@ -0,0 +1,47 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main12873 {
+
+    public static void main(String[] args)  throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       int N = Integer.parseInt(br.readLine());
+
+       List<Integer> list = new LinkedList<>();
+       for(int i = 1 ; i <=N;i++){
+           list.add(i);
+       }
+
+       int now = 0;
+       //한명 남겨야되니까 N 전까지 만
+       for(int i = 1; i< N ;i++){
+
+           int mod =  list.size();
+
+           //지울 인덱스번호
+
+          int index =(now+ ((i%mod * i %mod * i%mod) %mod)-1) %list.size();
+
+
+            if( index ==-1) {
+
+                index+=list.size();
+            }
+            list.remove(index);
+            now = index;
+
+
+       }
+
+       System.out.println(list.get(0));
+
+    }
+}

6368d8882b451454aae3090917e0f335ca0a1d93
canbe159 <canbe159@gmail.com>
2023-05-28 23:39:12 +0900

    이항계수 2 - [조합론 , 다이나믹 프로그래밍]

---
+++ src/Silver/Main11051.java
@@ -0,0 +1,21 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * 이항 계수 2
+ */
+
+public class Main11051 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+
+}

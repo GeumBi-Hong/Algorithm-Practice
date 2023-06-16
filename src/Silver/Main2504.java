87746750b3cff4658d5646402f93aba075b3f9a3
canbe159 <canbe159@gmail.com>
2023-06-16 20:43:14 +0900

    괄호의 값  - [자료구조, 스택]

--- src/Silver/Main2504.java
+++ src/Silver/Main2504.java
@@ -3,117 +3,82 @@ package Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
+/**
+ * 괄호의 값
+ */
 public class Main2504 {
 
-    static String []str;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String inputStr = br.readLine();
-        int lenght = inputStr.length();
 
-         str = new String[inputStr.length()];
+        // ex ) (()[[]])([])
+        String str = br.readLine();
 
-        for(int i = 0 ; i < str.length;i++){
-            str[i]=String.valueOf(inputStr.charAt(i));
-        }
-
-        int dif =1;
-
-        while (dif<lenght){
-
-            for(int i = 0 ; i<lenght-dif;i++){
-                if(str[i].equals("(")&&str[i+dif].equals(")")){
-                    int n = makeSum(i,i+dif,str[i]);
-                    if(n==-1){
-                      System.out.println(0);
-                        return;
-                    }
-                    if(dif==1){
-                      str[i]=String.valueOf(n);
-                    }else {
-                        str[i]=String.valueOf(n*2);
-                    }
-                }
-
-
-                if(str[i].equals("[")&&str[i+dif].equals("]")){
-                    int n = makeSum(i,i+dif,str[i]);
-                    if(n==-1){
-                        System.out.println(0);
-                        return;
-                    }
-
-                    if(dif==1){
-                        str[i]=String.valueOf(n);
-                    }else {
-
-                        str[i]=String.valueOf(n*3);
-                    }
-                }
-
-                }//for
-            dif++;
-            }
-
-        //다하고 나서 남은 괄호가 있는지 검사
+        //정답을 출력할 변수
         int answer = 0;
-        for(int i = 0 ; i<str.length;i++){
 
-            if(notInt(str[i])){
-                System.out.println(0);
-                return;
+
+        // 계산값을 저장할 변수
+        int preCount = 1;
+
+        Stack<Character> stack = new Stack<>();
+
+        //str 문자열 길이만큼 반복한다.
+        for (int i = 0; i < str.length(); i++) {
+
+            //i번째 문자
+            char bracket = str.charAt(i);
+
+            // 여는 괄호 기호일경우
+            if (bracket == '(' || bracket == '[') {
+                //스택에 넣는다.
+
+                stack.push(bracket);
+
+                preCount *= bracket == '(' ? 2 : 3;
+
+                //닫는 괄호
+            } else if (bracket == ')') {
+
+                //안되는 경우를 먼저 처리
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    //계산해 놨던 값 0
+                    answer = 0;
+                    break;
+                }
+
+                //올바른 괄호가 들어왔다면 누적합
+                if (i - 1 >= 0 && str.charAt(i - 1) == '(') {
+                    answer += preCount;
+                }
+                stack.pop();
+                preCount /= 2;
+
+            } else {
+                //안되는 조건 처리
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    answer = 0;
+                    break;
+                }
+
+                //올바른 괄호 [
+                if (i - 1 >= 0 && str.charAt(i - 1) == '[') {
+                    answer += preCount;
+                }
+                stack.pop();
+                preCount /= 3;
             }
-            answer+=Integer.parseInt(str[i]);
-        }
-
-
-
-       // printArray();
-           System.out.println(answer);
-        }
-
-
-
-private static void printArray (){
-        for(int i = 0  ; i<str.length;i++){
-            System.out.print(str[i]+" ");
-        }
-        System.out.println();
-}
-
-    private static int makeSum(int start , int end ,String s){
-        int sum=0;
-
-        if(end-start==1){
-            if(s.equals("(")){
-                str[end]="0";
-                return 2;
-            }
-
-            if(s.equals("[")){
-
-                str[end]="0";
-                return 3;
-            }
 
         }
-        for(int i=start+1 ; i<=end-1;i++){
-            if(notInt(str[i])){
-                return -1;
-            }
-            sum+=Integer.parseInt(str[i]);
-            str[i]="0";
-        }
-        str[end]="0";
-        return sum;
+
+        if(!stack.isEmpty()) answer = 0;
+
+        System.out.println(answer);
+
     }
 
-    private static boolean notInt(String s){
-        if( s.equals("(") || s.equals(")") || s.equals("[") || s.equals("]")){
-          return true;
-        }
-        return false;
-    }
 }

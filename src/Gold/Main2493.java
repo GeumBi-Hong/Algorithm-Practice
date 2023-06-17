6f181c8ecd1f8d443f7746c4d0a338c38c941e63
canbe159 <canbe159@gmail.com>
2023-06-17 17:49:32 +0900

    탑  - [자료구조, 스택]

--- src/Gold/Main2493.java
+++ src/Gold/Main2493.java
@@ -7,12 +7,15 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * 탑
+ */
 public class Main2493 {
 
-    static class Info   {
+    static class Info{
+
         int height; //탑의 높이
         int index; // 몇번 째 탑이 였는지
-
         public Info(int height,int index){
             this.height=height;
             this.index=index;
@@ -32,28 +35,39 @@ public class Main2493 {
         });
 
         int N = Integer.parseInt(br.readLine());
+
         //탑 높이
+        // 6 9 5 7 4
+        // 4 5
         int [] height = new int[N+1];
         //i번째 탑의 수신을 몇번째 탑이 받았는지 저장하기 위한 배열
         int [] top = new int[N+1];
 
         /** 입력값 저장 */
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i <=N;i++){
+
+        for(int i = 1 ; i <= N ;i++){
+
             height[i] = Integer.parseInt(st.nextToken());
         }
 
-
         priorityQueue.add(new Info(height[N],N));
-        for(int i = N-1; i >=1; i--){
+
+        //탑의 개수 만큼 반복
+        for(int i = N-1; i >= 1; i--){
 
             priorityQueue.add(new Info(height[i],i));
-           while (!priorityQueue.isEmpty()){
-               Info  info = priorityQueue.peek();
+
+           while (!priorityQueue.isEmpty()) {
+
+               Info info = priorityQueue.peek();
+
                //큐에 들어가 있는 탑의 높이보다 크다면
-               if(height[i]>info.height){
+               if(height[i] > info.height){
+
                    top[info.index]=i;
                    priorityQueue.poll();
+
                }else {
                    break;
                }
@@ -62,13 +76,9 @@ public class Main2493 {
         }
 
         StringBuilder sb = new StringBuilder();
-
         for(int i = 1 ; i<=N;i++){
             sb.append(top[i]+" ");
         }
-
         System.out.println(sb);
-
-
     }
 }

d1150f7851d5a146a703aa68ca4c73ebd1c32d75
canbe159 <canbe159@gmail.com>
2023-06-26 23:54:28 +0900

    문제 추천 시스템 Version 1

---
+++ src/Gold/Main21939.java
@@ -0,0 +1,35 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * 문제 추천 시스템 Version 1
+ *
+ * [문제 조건]
+ * - recommend 1 : 추천 문제 리스트에서 가장 어려운 문제 번호 출력 -> 만약 그 문제가 여러개라면 문제 번호가 큰것 출력
+ * - recommend -1 : 추천 문제 리스트에서 가장 쉬운 문제 번호 출력 -> 만약 그 문제가 여러개라면 문제 번호가 작은것 출력
+ *
+ * - add P L : 추천 문제 리스트에 난이도가 L인 문제 번호 P를 추가 , (이때 추천 문제 리스트에 없는 문제번호 P만 입력으로 주어짐, 이전에 있던 문제번호가 다른 난이도로 올 수 있음)
+ *
+ * [풀이]
+ * - 걍 정렬 조건 대로 구현
+ *
+ */
+public class Main21939 {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+
+
+
+    }
+}

479ca0d47ccb32bcfb74361d58cccb83334e2920
canbe159 <canbe159@gmail.com>
2023-08-06 23:59:14 +0900

    파일 합치기 3 - [그리디 알고리즘]

--- src/Gold/Main13975.java
+++ src/Gold/Main13975.java
@@ -1,48 +1,68 @@
 package Gold;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * 파일 합치기 3
+ *
+ * [문제 풀이]
+ * 이거 백준 1715번이랑 비슷한거같은데
+ * 합칠때 있는 파일 중에 가장 작은 두개를 골라서 합쳐야 가장 최소 비용이 나온다
+ */
+
 public class Main13975 {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        int testCase = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        while (T-->0){
-            //합을 구할때 int 범위를 넘어갈 수 있으므로 long
-            PriorityQueue<Long> priorityQueue  = new PriorityQueue<Long>();
 
-            int n = Integer.parseInt(br.readLine());
+        //테스트 케이스 수 만큼 반복
+        while (testCase-- > 0) {
+
+            int N = Integer.parseInt(br.readLine());
+
+            PriorityQueue<Long> queue = new PriorityQueue<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            while (st.hasMoreTokens()){
-                priorityQueue.add(Long.parseLong(st.nextToken()));
-
+            //우선순위 큐로 오름 차순 정렬
+            while (N-- > 0) {
+                queue.add(Long.parseLong(st.nextToken()));
             }
 
-            Long sum =(long) 0;
 
-            //2개를 꺼낼꺼니까 사이즈가 2이상일때만 꺼냄
-            while (priorityQueue.size()>1){
-                Long n1 = priorityQueue.poll();
-                Long n2 = priorityQueue.poll();
+            //파일의 최소 합
+            long answer = 0L;
 
-                sum+=n1+n2;
-                priorityQueue.add(n1+n2);
+            while (!queue.isEmpty()) {
+
+                long numA = queue.poll();
+
+                if (queue.isEmpty()) {
+                    break;
+                }
+
+                long numB = queue.poll();
+
+                answer += numA + numB;
+                queue.add(numA + numB);
             }
 
-            sb.append(sum).append("\n");
-
+            sb.append(answer).append("\n");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
 
     }
+
+
 }
